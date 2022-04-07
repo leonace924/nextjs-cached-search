c8c5951661a7769df2bfd95c3ad0d1582d6af73f
leonace924 <meobius123@gmail.com>
2022-04-07 14:43:56 -0400

    fix: update files

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'https://athena-7.herokuapp.com/ancients.json';
+export const BASE_URL = 'https://athena-7.herokuapp.com/ancients.json';
 // Search Endpoint: https://athena-7.herokuapp.com/ancients.json?search=Ath
 // Error Endpoint: https://athena-7.herokuapp.com/ancients.json?error=true
